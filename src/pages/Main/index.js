93008709af13f90c4db62d591c8690673827c135
Pjeferson <paulojeferson34@gmail.com>
2020-01-11 14:01:20 -0300

    getting products from api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,96 +1,51 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
-export default function Main() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
-          alt="Tênis"
-        />
-        <strong>Descrição</strong>
-        <span>Preço</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      price: formatPrice(product.price),
+    }));
+
+    this.setState({
+      products: data,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    console.log(products);
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt="Tênis" />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
