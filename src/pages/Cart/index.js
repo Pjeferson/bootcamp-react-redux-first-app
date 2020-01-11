2d2ed8cbc805f7dc52bab16ab76e605063fdcd8a
Pjeferson <paulojeferson34@gmail.com>
2020-01-10 22:11:43 -0300

    implementing Cart page structure

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://cdn.shopify.com/s/files/1/0248/4282/9906/products/4j_2048x.jpg?v=1563085766"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <button type="button">
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+              <input type="number" readOnly value={2} />
+              <button type="button">
+                <MdAddCircleOutline size={20} color="7159c1" />
+              </button>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
